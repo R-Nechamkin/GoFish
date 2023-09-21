1c4ab3cb99ae0bbb99ae3d41babe7af26a4f051a
R-Nechamkin <118650276+R-Nechamkin@users.noreply.github.com>
2023-09-21 00:03:31 -0400

    fixed up initial fields and methods

--- src/goFish/Player.java
+++ src/goFish/Player.java
@@ -1,11 +1,13 @@
 package goFish;
 
+import java.util.List;
+
 public interface Player {
   private String name;
   private List deck;
 
   public String getName();
   public List<Card> getHand();
-  public addToHand(Card);
-  public play();
+  public void addToHand(Card);
+  public void play();
 }
