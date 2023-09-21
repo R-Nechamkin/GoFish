ae68e27330fac2852660f5bbbd56c52817ee6c96
R-Nechamkin <118650276+R-Nechamkin@users.noreply.github.com>
2023-09-20 23:22:22 -0400

    added fields and methods

--- src/goFish/Player.java
+++ src/goFish/Player.java
@@ -2,5 +2,10 @@ package goFish;
 
 public interface Player {
   private String name;
+  private List deck;
 
+  public String getName();
+  public List<Card> getHand();
+  public addToHand(Card);
+  public play();
 }
