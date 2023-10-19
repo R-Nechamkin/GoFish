d22473f16e5b9921d6f27548b81651eefcfd21dd
hindylondon <119772445+hindylondon@users.noreply.github.com>
2023-10-19 19:15:59 -0400

    Update Card-added code for getSuit().java

--- src/GoFishGameDev/Card.java
+++ src/GoFishGameDev/Card.java
@@ -3,12 +3,18 @@ import java.util.*;
 public interface Card {
 	 String getRank(){
 		String[] deckNums = "A,2,3,4,5,6,7,8,9,10,J,Q,K".split(",");
-		 Random ran = new Random();
-    		int randomNumber = ran.nextInt(deckNums.length);
+		 Random randRank = new Random();
+    		int randomNumber = randRank.nextInt(deckNums.length);
    		 String rank = deckNums[randomNumber];
    		 System.out.println(rank);
 	 }
-	 String getSuit();
+	 String getSuit(){
+	 String[] deckSuit = "Hearts,Clubs,Spades,Diamonds".split(",");
+		 Random randSuit = new Random();
+    		int randomNumber = randSuit.nextInt(deckSuit.length);
+   		 String suit = deckSuit[randomNumber];
+   		 System.out.println(suit);
+	 }
 	 String toString();
 
 }
