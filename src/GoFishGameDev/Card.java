eb568ad20e93b8721cd472228c2d42075cea079e
hindylondon <119772445+hindylondon@users.noreply.github.com>
2023-10-19 19:02:53 -0400

    Update Card-added code for getRank().java

--- src/GoFishGameDev/Card.java
+++ src/GoFishGameDev/Card.java
@@ -1,7 +1,13 @@
 package GoFishGameDev;
-
+import java.util.*;
 public interface Card {
-	 String getRank();
+	 String getRank(){
+		String[] deckNums = "A,2,3,4,5,6,7,8,9,10,J,Q,K".split(",");
+		 Random ran = new Random();
+    		int randomNumber = ran.nextInt(deckNums.length);
+   		 String rank = deckNums[randomNumber];
+   		 System.out.println(rank);
+	 }
 	 String getSuit();
 	 String toString();
 
