919fec0cd288e9861b0051a717debac2492dd743
hindylondon <119772445+hindylondon@users.noreply.github.com>
2023-10-22 22:37:28 -0400

    Update Card.java

--- src/GoFishGameDev/Card.java
+++ src/GoFishGameDev/Card.java
@@ -1,24 +1,24 @@
 package GoFishGameDev;
-import java.util.*;
-public interface Card {
+public Class Card {
+	private String rank;
+	private String suit;
+	
+	public Card(String rank, String suit) {
+		this.rank = rank;
+		this.suit = suit;
+	}
+	
 	String getRank() {
-		String[] deckNums = "A,2,3,4,5,6,7,8,9,10,J,Q,K".split(",");
-		Random randRank = new Random();
-		int randomNumber = randRank.nextInt(deckNums.length);
-		String rank = deckNums[randomNumber];
-		return rank;
+		return this.rank;
 	}
 
 	String getSuit() {
-		String[] deckSuit = "Hearts,Clubs,Spades,Diamonds".split(",");
-		Random randSuit = new Random();
-		int randomNumber = randSuit.nextInt(deckSuit.length);
-		String suit = deckSuit[randomNumber];
-		return suit;
+
+		return this.suit
 	}
 
 	String toString() {
-		return "Card" + getRank() + " of " + getSuit();
+		return this.rank + " of " + this.suit;
 	}
 	
 
