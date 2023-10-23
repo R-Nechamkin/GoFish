7153996be80c7293b188481caf0bad3feca02ece
hindylondon <119772445+hindylondon@users.noreply.github.com>
2023-10-22 23:12:49 -0400

    Update Game-added comments.java

--- src/GoFishGameDev/Game.java
+++ src/GoFishGameDev/Game.java
@@ -1,6 +1,7 @@
 package GoFishGameDev;
 
 public interface Game {
+//interface for both humanPlayer and compPlayer to play game from
 	void startGame();
     boolean isGameOver();
     void playTurn(Player player);
