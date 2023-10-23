7ddbc9cc6af7d45f661ba5ee1fec29e2fd5cd78f
hindylondon <119772445+hindylondon@users.noreply.github.com>
2023-10-22 22:38:05 -0400

    Update Deck.java

--- src/GoFishGameDev/Deck.java
+++ src/GoFishGameDev/Deck.java
@@ -1,19 +1,30 @@
 package GoFishGameDev;
 
-public interface Deck {
-
-	String[] deck = new String[52]
-
-	Card drawCard() {
-		Card currentCard = new Card();
-		while (deck.includes(currentCard)) {
-			Card currentCard = new Card();
+public Class Deck {
+	private List<Card>cards;
+	private int numCards = 0;
+	
+	String[] deckSuit = "Hearts,Clubs,Spades,Diamonds".split(",");
+	String[] deckRank = "A,2,3,4,5,6,7,8,9,10,J,Q,K".split(",");
+	
+	public Deck() {
+		cards = new ArrayList<Card>();
+		for (String suit : deckSuit) {
+			for (String rank : deckRank) {
+				Card card = new Card(deckRank, deckSuit);
+				cards.add(card);
+				cardCount ++;
+			}
 		}
-		deck.add(currentCard);
 	}
 
 	void shuffleDeck();{
-		deck.clear();
+		Collections.shuffle(cards);
 	}
+	
+	public cardCount{
+		return cardCount;
+	}
+	
 
 }
