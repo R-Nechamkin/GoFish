2abc6a1b95fe4d9a5678a5b4351e9eb7085357b3
R-Nechamkin <118650276+R-Nechamkin@users.noreply.github.com>
2023-10-22 23:16:42 -0400

    Update Deck.java

--- src/GoFishGameDev/Deck.java
+++ src/GoFishGameDev/Deck.java
@@ -1,8 +1,11 @@
 package GoFishGameDev;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Deck {
 	private List<Card>cards;
-	private int numCards = 0;
+	private int cardCount = 0;
 	
 	String[] deckSuit = "Hearts,Clubs,Spades,Diamonds".split(",");
 	String[] deckRank = "A,2,3,4,5,6,7,8,9,10,J,Q,K".split(",");
@@ -11,18 +14,18 @@ public class Deck {
 		cards = new ArrayList<Card>();
 		for (String suit : deckSuit) {
 			for (String rank : deckRank) {
-				Card card = new Card(deckRank, deckSuit);
+				Card card = new Card(suit, rank);
 				cards.add(card);
 				cardCount ++;
 			}
 		}
 	}
 
-	void shuffleDeck();{
+	public void shuffleDeck(){
 		Collections.shuffle(cards);
 	}
 	
-	public cardCount{
+	public int cardCount(){
 		return cardCount;
 	}
 	
