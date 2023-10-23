64f95887cb084949d7e6755fcfbd21bc5de86dc3
hindylondon <119772445+hindylondon@users.noreply.github.com>
2023-10-22 23:13:37 -0400

    Update Player-added comments.java

--- src/GoFishGameDev/Player.java
+++ src/GoFishGameDev/Player.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Set;
 
 public interface Player {
+	//interface for players, comp or human
 	String getName();
     List<Card> getHand();
     Set<String> getMatches();
