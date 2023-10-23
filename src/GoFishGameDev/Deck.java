f0d300a33cd17bfbd043437bb964b2f4d25753bb
hindylondon <119772445+hindylondon@users.noreply.github.com>
2023-10-22 22:56:43 -0400

    Update Deck-added comments.java

--- src/GoFishGameDev/Deck.java
+++ src/GoFishGameDev/Deck.java
@@ -1,13 +1,15 @@
 package GoFishGameDev;
 
-public Class Deck {
+public class Deck {
+	//fields
 	private List<Card>cards;
 	private int numCards = 0;
-	
+	//set ranks and suits as arrays
 	String[] deckSuit = "Hearts,Clubs,Spades,Diamonds".split(",");
 	String[] deckRank = "A,2,3,4,5,6,7,8,9,10,J,Q,K".split(",");
-	
+	//constructor
 	public Deck() {
+		//build deck of cards
 		cards = new ArrayList<Card>();
 		for (String suit : deckSuit) {
 			for (String rank : deckRank) {
@@ -17,12 +19,12 @@ public Class Deck {
 			}
 		}
 	}
-
-	void shuffleDeck();{
+//shuffle deck
+	public void shuffleDeck();{
 		Collections.shuffle(cards);
 	}
-	
 	public cardCount{
+		//cardcounter
 		return cardCount;
 	}
 	
