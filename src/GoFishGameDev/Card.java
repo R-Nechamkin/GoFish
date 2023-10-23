bab56233d857a54d5d35d307b7738a408484382e
hindylondon <119772445+hindylondon@users.noreply.github.com>
2023-10-22 23:04:19 -0400

    Update Card-added comments.java

--- src/GoFishGameDev/Card.java
+++ src/GoFishGameDev/Card.java
@@ -1,22 +1,23 @@
 package GoFishGameDev;
 public class Card {
+//fields
 	private String rank;
 	private String suit;
-	
+//constructor
 	public Card(String rank, String suit) {
 		this.rank = rank;
 		this.suit = suit;
 	}
-	
+//getRank
 	public String getRank() {
 		return this.rank;
 	}
-
+//getSuit
 	public String getSuit() {
 
 		return this.suit;
 	}
-
+//toString
 	public String toString() {
 		return this.rank + " of " + this.suit;
 	}
